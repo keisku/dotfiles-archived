b1b1b319dd20e4312b435d4c62a4790b09d63223
梅垣圭佑 <umegakikeisuke@umegakikeisukenoMacBook-Pro.local>
2020-03-12 13:39:09 +0900

    update: install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ git submodule update --init --recursive
 # symlink dotfiles
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 

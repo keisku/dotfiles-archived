6ba2e1f830b7cf202420ee4ba2deb2a4b7aed703
kskumgk63 <keisuke.umegaki.630@gmail.com>
2020-04-01 16:13:15 +0900

    feature: script to install vscode extensions

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -5,7 +5,5 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -s ~/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/
-ln -s ~/dotfiles/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/
 
 source ~/dotfiles/.zshrc

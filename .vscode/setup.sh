aa9b8fc87f93697cae1157d74829340a1dc3fc53
kskumgk63 <keisuke.umegaki.630@gmail.com>
2020-10-02 21:40:54 +0900

    update setup.sh for vscode

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -2,16 +2,11 @@
 
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-rm "$VSCODE_SETTING_DIR/settings.json"
-ln -s "settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+rm $VSCODE_SETTING_DIR/settings.json
+ln -s settings.json $VSCODE_SETTING_DIR/settings.json
 
-rm "$VSCODE_SETTING_DIR/keybindings.json"
-ln -s "keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+rm $VSCODE_SETTING_DIR/keybindings.json
+ln -s keybindings.json $VSCODE_SETTING_DIR/keybindings.json
 
 # install extention
-cat extensions | while read line
-do
- code --install-extension $line
-done
-
-code --list-extensions > extensions
+cat extensions | xargs code --install-extension
